3808eed552a7767571b28b35667487f09b55ef2d
rishavtiwari22 <tiwaririshav463@gmail.com>
2025-01-21 23:37:05 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -7,12 +7,10 @@ module.exports = router;
 // Post Method
 router.post('/post', async (req, res) => {
     const data = new Model({
-        campus: req.body.campus,
-        date: req.body.date,  
-        house: req.body.house,
-        point: req.body.point,
-        rewards: req.body.rewards,
-        amount: req.body.amount
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        isAdmin: req.body.isAdmin
     })
     try {
         const dataToSave = await data.save();
@@ -25,25 +23,22 @@ router.post('/post', async (req, res) => {
 
 // Get all Method
 router.get('/getAll', async (req, res) => {
-    try {
+    try{
         const data = await Model.find();
         res.json(data)
     }
-    catch (error) {
+    catch(error){
         res.status(500).json({message: error.message})
     }
 })
 
 // Get by ID Method
-router.get('/getOne/:id', async (req, res) => {
-    try {
+router.get('/getOne/:id',async (req, res)=>{
+    try{
         const data = await Model.findById(req.params.id);
-        if (!data) {
-            return res.status(404).json({message: 'Record not found'})
-        }
         res.json(data)
     }
-    catch (error) {
+    catch(error){
         res.status(500).json({message: error.message})
     }
 })
@@ -72,12 +67,9 @@ router.delete('/delete/:id', async (req, res) => {
     try {
         const id = req.params.id;
         const data = await Model.findByIdAndDelete(id)
-        if (!data) {
-            return res.status(404).json({message: 'Record not found'})
-        }
-        res.json({message: `Document with ID ${id} has been deleted`})
+        res.send(`Document with ${data.name} has been deleted..`)
     }
     catch (error) {
-        res.status(400).json({message: error.message})
+        res.status(400).json({ message: error.message })
     }
 })
